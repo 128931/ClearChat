ae648ba631f98c820187be9d7be4e39dd5c6380e
128931 <101839340+128931@users.noreply.github.com>
2022-03-28 00:02:20 -0700

    Allow for clearing certain players chat's as well

--- src/main/java/me/onetwoeight/clearchat/ClearChat.java
+++ src/main/java/me/onetwoeight/clearchat/ClearChat.java
@@ -6,16 +6,31 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import static org.bukkit.Bukkit.broadcastMessage;
+import static org.bukkit.Bukkit.getPlayer;
 
 public final class ClearChat implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender.hasPermission("cc.clear")) {
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+        if (sender.hasPermission("cc.global") && args.length == 0) {
             for (int i = 0; i < 1000; i++)
                 broadcastMessage("");
             broadcastMessage(ChatColor.DARK_AQUA + "Chat Cleared by " + sender.getName() + ChatColor.RESET);
-        } else sender.sendMessage(ChatColor.RED + "You do not have permission to do that." + ChatColor.RESET);
+        } else if (sender.hasPermission("cc.player") && args.length == 1) {
+            for (int i = 0; i < 1000; i++) {
+                if (getPlayer(args[0]) != null && getPlayer(args[0]).isOnline())
+                    getPlayer(args[0]).sendMessage("");
+                else {
+                    sender.sendMessage(ChatColor.RED + "Could not find specified player" + ChatColor.RESET);
+                    break;
+                }
+            }
+            if (getPlayer(args[0]) != null && getPlayer(args[0]).isOnline())
+                getPlayer(args[0]).sendMessage(ChatColor.DARK_AQUA + "Your Chat Was Cleared by " + sender.getName() + ChatColor.RESET);
+        } else if (!sender.hasPermission("cc.global") && args.length == 0 || !sender.hasPermission("cc.player") && args.length > 0)
+            sender.sendMessage(ChatColor.RED + "You do not have permission to do that" + ChatColor.RESET);
+        else if (sender.hasPermission("cc.player") && args.length > 1)
+            sender.sendMessage(ChatColor.RED + "Please refrain from using 2 or more args" + ChatColor.RESET);
         return true;
     }
 }
