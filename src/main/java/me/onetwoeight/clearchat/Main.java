44676d3e543a125b5ebe625bfd64a168f42e5964
128931 <101839340+128931@users.noreply.github.com>
2022-03-30 17:31:27 -0700

    Static imports wooo

--- src/main/java/me/onetwoeight/clearchat/Main.java
+++ src/main/java/me/onetwoeight/clearchat/Main.java
@@ -3,7 +3,7 @@ package me.onetwoeight.clearchat;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+import static java.util.Objects.requireNonNull;
 
 public final class Main extends JavaPlugin {
 
@@ -14,7 +14,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        Objects.requireNonNull(getCommand("cc"), "Executor must not be null").setExecutor(new ChatListener(this));
+        requireNonNull(getCommand("cc"), "Executor must not be null").setExecutor(new ChatListener(this));
         getLogger().info(name + " v" + version + " Enabled");
     }
 
