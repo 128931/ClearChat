1f8659360e8e5e3460ef006c2bfc2a472962ebe5
128931 <101839340+128931@users.noreply.github.com>
2022-04-14 08:29:41 -0700

    Update ChatListener.kt

--- src/main/kotlin/me/onetwoeight/clearchat/ChatListener.kt
+++ src/main/kotlin/me/onetwoeight/clearchat/ChatListener.kt
@@ -8,12 +8,13 @@ import org.bukkit.command.CommandSender
 
 internal class ChatListener(private val plugin: Main) : CommandExecutor {
 
+    private val prefix = "Prefix"
+    private val send = "%sender%"
+    private val global = "cc.global"
+    private val player = "cc.player"
+
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        val prefix = "Prefix"
-        val send = "%sender%"
-        val globalperm = "cc.global"
-        val playerperm = "cc.player"
-        if (sender.hasPermission(globalperm) && args.isEmpty()) {
+        if (sender.hasPermission(global) && args.isEmpty()) {
             for (i in 0..999) {
                 Bukkit.broadcastMessage("")
             }
@@ -23,7 +24,7 @@ internal class ChatListener(private val plugin: Main) : CommandExecutor {
                     plugin.config.getString(prefix) + plugin.config.getString("Global")
                 ).replace(send, sender.name)
             )
-        } else if (sender.hasPermission(playerperm) && args.size == 1) {
+        } else if (sender.hasPermission(player) && args.size == 1) {
             for (i in 0..999) {
                 if (Bukkit.getPlayer(args[0])?.isOnline == true) {
                     Bukkit.getPlayer(args[0])?.sendMessage("")
@@ -52,7 +53,7 @@ internal class ChatListener(private val plugin: Main) : CommandExecutor {
                     )
                 )
             }
-        } else if (!sender.hasPermission(globalperm) && args.isEmpty() || !sender.hasPermission(playerperm) && args.isNotEmpty()) {
+        } else if (!sender.hasPermission(global) && args.isEmpty() || !sender.hasPermission(player) && args.isNotEmpty()) {
             sender.sendMessage(
                 plugin.config.getString("NoPermission")?.let {
                     ChatColor.translateAlternateColorCodes(
@@ -60,7 +61,7 @@ internal class ChatListener(private val plugin: Main) : CommandExecutor {
                     )
                 }
             )
-        } else if (sender.hasPermission(playerperm) && args.size > 1) {
+        } else if (sender.hasPermission(player) && args.size > 1) {
             sender.sendMessage(ChatColor.RED.toString() + "Please refrain from using 2 or more args" + ChatColor.RESET)
         }
         return true
