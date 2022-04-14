88de889b855b6a4e9dd71f4d1ebacf8772414277
128931 <101839340+128931@users.noreply.github.com>
2022-04-14 15:27:59 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    id("org.sonarqube") version ("3.3")
 }
 
 group = "me.onetwoeight"
@@ -41,5 +42,13 @@ tasks {
     }
     test {
         useJUnitPlatform()
+    }
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "128931_ClearChat")
+        property("sonar.organization", "128931")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
