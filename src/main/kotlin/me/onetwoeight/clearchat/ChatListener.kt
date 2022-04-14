cd328b7b408cd0311f62900a468f3a1a5641105d
128931 <101839340+128931@users.noreply.github.com>
2022-04-14 08:28:12 -0700

    [/] Cleanup

--- src/main/kotlin/me/onetwoeight/clearchat/ChatListener.kt
+++ src/main/kotlin/me/onetwoeight/clearchat/ChatListener.kt
@@ -9,17 +9,21 @@ import org.bukkit.command.CommandSender
 internal class ChatListener(private val plugin: Main) : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        if (sender.hasPermission("cc.global") && args.isEmpty()) {
+        val prefix = "Prefix"
+        val send = "%sender%"
+        val globalperm = "cc.global"
+        val playerperm = "cc.player"
+        if (sender.hasPermission(globalperm) && args.isEmpty()) {
             for (i in 0..999) {
                 Bukkit.broadcastMessage("")
             }
             Bukkit.broadcastMessage(
                 ChatColor.translateAlternateColorCodes(
                     '&',
-                    plugin.config.getString("Prefix") + plugin.config.getString("Global")
-                ).replace("%sender%", sender.name)
+                    plugin.config.getString(prefix) + plugin.config.getString("Global")
+                ).replace(send, sender.name)
             )
-        } else if (sender.hasPermission("cc.player") && args.size == 1) {
+        } else if (sender.hasPermission(playerperm) && args.size == 1) {
             for (i in 0..999) {
                 if (Bukkit.getPlayer(args[0])?.isOnline == true) {
                     Bukkit.getPlayer(args[0])?.sendMessage("")
@@ -32,8 +36,8 @@ internal class ChatListener(private val plugin: Main) : CommandExecutor {
                 Bukkit.getPlayer(args[0])?.sendMessage(
                     ChatColor.translateAlternateColorCodes(
                         '&',
-                        plugin.config.getString("Prefix") + plugin.config.getString("Player")?.replace(
-                            "%sender%",
+                        plugin.config.getString(prefix) + plugin.config.getString("Player")?.replace(
+                            send,
                             sender.name
                         )
                     )
@@ -41,22 +45,22 @@ internal class ChatListener(private val plugin: Main) : CommandExecutor {
                 sender.sendMessage(
                     ChatColor.translateAlternateColorCodes(
                         '&',
-                        plugin.config.getString("Prefix") + plugin.config.getString("Success")?.replace(
+                        plugin.config.getString(prefix) + plugin.config.getString("Success")?.replace(
                             "%person%",
                             args[0]
                         )
                     )
                 )
             }
-        } else if (!sender.hasPermission("cc.global") && args.isEmpty() || !sender.hasPermission("cc.player") && args.isNotEmpty()) {
+        } else if (!sender.hasPermission(globalperm) && args.isEmpty() || !sender.hasPermission(playerperm) && args.isNotEmpty()) {
             sender.sendMessage(
                 plugin.config.getString("NoPermission")?.let {
                     ChatColor.translateAlternateColorCodes(
-                        '&', it.replace("%sender%", sender.name)
+                        '&', it.replace(send, sender.name)
                     )
                 }
             )
-        } else if (sender.hasPermission("cc.player") && args.size > 1) {
+        } else if (sender.hasPermission(playerperm) && args.size > 1) {
             sender.sendMessage(ChatColor.RED.toString() + "Please refrain from using 2 or more args" + ChatColor.RESET)
         }
         return true
--- src/main/kotlin/me/onetwoeight/clearchat/Main.kt
+++ src/main/kotlin/me/onetwoeight/clearchat/Main.kt
@@ -4,17 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
 
-    private val file = description
-    private val version = file.version
-    private val filename = file.name
-
     override fun onEnable() {
         saveDefaultConfig()
         getCommand("cc")?.setExecutor(ChatListener(this))
-        logger.info("$filename v$version Enabled")
+        logger.info("${description.name} v${description.version} Enabled")
     }
 
     override fun onDisable() {
-        logger.info("$filename v$version Disabled")
+        logger.info("${description.name} v${description.version} Disabled")
     }
 }
