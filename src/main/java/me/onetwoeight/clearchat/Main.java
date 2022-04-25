03bdeebbda1d9a03e122123c292c158b4b8ec5a1
128931 <101839340+128931@users.noreply.github.com>
2022-04-25 16:17:14 -0700

    [/] Start of moving over to null checks
    [/] I rather do nothing then throw null pointer exceptions if the command is null

--- src/main/java/me/onetwoeight/clearchat/Main.java
+++ src/main/java/me/onetwoeight/clearchat/Main.java
@@ -2,10 +2,9 @@ package me.onetwoeight.clearchat;
 
 import me.onetwoeight.clearchat.listeners.ChatListener;
 import me.onetwoeight.clearchat.statistics.Metrics;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * @author onetwoeight
@@ -20,7 +19,10 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        requireNonNull(getCommand("cc"), "Expression 'getCommand(\"cc\")' must not be null").setExecutor(new ChatListener(this));
+        PluginCommand pluginCommand = getCommand("cc");
+        if (pluginCommand != null) {
+            pluginCommand.setExecutor(new ChatListener(this));
+        }
         new Metrics(this, 14968);
         getLogger().info(() -> name + " v" + version + " Enabled"); // since Java 8, we can use Supplier, which will be evaluated lazily
     }
