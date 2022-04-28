dfaf481840ffcdf0ca63c5396596df6d9a1e4176
128931 <101839340+128931@users.noreply.github.com>
2022-04-27 18:51:18 -0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+    id("org.sonarqube") version "3.3"
+}
+
+group = "me.onetwoeight"
+version = "1.0"
+
+val kotlinJvmTarget = JavaVersion.toVersion(8)
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+        name = "spigotmc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains:annotations:23.0.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation(kotlin("test"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = kotlinJvmTarget.toString()
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = kotlinJvmTarget.toString()
+        }
+    }
+    processResources {
+        filesMatching("plugin.yml") {
+            expand("version" to project.version)
+        }
+    }
+    test {
+        useJUnitPlatform()
+    }
+    jar {
+        val dependencies = configurations.runtimeClasspath.get().map(::zipTree)
+        from(dependencies)
+        // Due to never using the Kotlin Reflect library in this project we can exclude these without any issues/errors occurring
+        exclude(
+            "**/*.kotlin_metadata",
+            "**/*.kotlin_module",
+            "**/*.kotlin_builtins",
+            "META-INF/maven/**",
+            "META-INF/versions/**"
+        )
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "128931_ClearChat")
+        property("sonar.organization", "128931")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
