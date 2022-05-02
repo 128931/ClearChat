583de70bb1bf2c8f52929dec96e177b7b43ce9b6
128931 <101839340+128931@users.noreply.github.com>
2022-05-02 00:19:58 -0700

    [/] Made the update message more easy to see

--- src/main/kotlin/io/github/onetwoeight/clearchat/ClearChatPlugin.kt
+++ src/main/kotlin/io/github/onetwoeight/clearchat/ClearChatPlugin.kt
@@ -2,7 +2,9 @@ package io.github.onetwoeight.clearchat
 
 import io.github.onetwoeight.clearchat.listeners.ChatListener
 import io.github.onetwoeight.clearchat.statistics.Metrics
+import io.github.onetwoeight.clearchat.utilities.CC
 import io.github.onetwoeight.clearchat.utilities.UpdateChecker
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -15,14 +17,15 @@ class ClearChatPlugin : JavaPlugin() {
         saveDefaultConfig()
         getCommand("cc")?.setExecutor(ChatListener(this))
         Metrics(this, 14_968)
-        logger.info("${description.name} v${description.version} Enabled")
         UpdateChecker(101_734).getLatestVersion { version: String ->
             if (description.version.equals(version, ignoreCase = true)) {
                 logger.info("The plugin is up to date.")
             } else {
-                logger.warning("The plugin has been updated; please get it from https://spigotmc.org/resources/clearchat.101734/")
+                Bukkit.getConsoleSender()
+                    .sendMessage(CC.translate("[${description.name}] &cThe plugin has been updated; please download it at https://spigotmc.org/resources/clearchat.101734/&r"))
             }
         }
+        logger.info("${description.name} v${description.version} Enabled")
     }
 
     override fun onDisable() {
